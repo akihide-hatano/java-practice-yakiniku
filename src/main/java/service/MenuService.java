644bd3ccf93ad6303a600850eb57d7a28e011b8a
akihide-hatano <a.h.19841202@gmail.com>
2026-04-04 21:53:04 +0900

    MenuService.javaにて、MenuServiceクラスを作成し、Yakinikuyaクラスのインスタンスを生成して、メニューアイテムを追加するコードを追加しました。

---
+++ src/main/java/service/MenuService.java
@@ -0,0 +1,55 @@
+package service;
+
+//importを追加
+import model.MenuItem ;
+import model.Restaurant;
+import model.Yakinikuya;
+
+public class MenuService {
+
+    public boolean addCommonMenuItem(Yakinikuya yakinikuya,String name,int price){
+        //
+        MenuItem[] commoMenuItems = yakinikuya.getCommonMenuItems();
+
+        //
+        for (int i = 0; i < commoMenuItems.length; i++) {
+            if(commoMenuItems[i] == null){
+                commoMenuItems[i] = new MenuItem(name, price);
+                return true;
+            }
+        }
+        throw new IllegalStateException("共通メニューをこれ以上追加できません");
+    }
+
+    public boolean addLocalMenuItem(Restaurant restaurant, String name, int price){
+        //
+        MenuItem[] localMenuItems = restaurant.getLocalMenuItems();
+
+        //
+        for (int i = 0; i < localMenuItems.length; i++) {
+            if(localMenuItems[i] == null){
+                localMenuItems[i] = new MenuItem(name, price);
+                return true;
+            }
+        }
+        throw new IllegalStateException("店舗メニューをこれ以上追加できません");
+    }
+
+    public void displayCommonMenu(Yakinikuya yakinikuya){
+        System.out.println();
+        for (MenuItem item : yakinikuya.getCommonMenuItems()) {
+            if( item != null){
+                System.out.println(item.getName() + " : " + item.getPrice());
+            }
+        }
+    }
+
+    public void displayLocalMenu(Restaurant restaurant){
+        System.out.println();
+        for (MenuItem item : restaurant.getLocalMenuItems()){
+            if( item != null){
+                System.out.println(item.getName() + " :" + item.getPrice());
+            }
+        }
+    }
+}
