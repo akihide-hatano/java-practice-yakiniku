c9eb002bee7a6ca32e995f1f434c7dd1754becc6
akihide-hatano <a.h.19841202@gmail.com>
2026-04-04 16:44:27 +0900

    MenuItem.javaにて、コンストラクタの引数の型をStringからintに変更し、ゲッターもそれに合わせて変更しました。

--- src/main/java/model/MenuItem.java
+++ src/main/java/model/MenuItem.java
@@ -1,20 +1,22 @@
 package model;
 
 public class MenuItem {
-
+	//フィールドで必要なものを定義
 	private final String name;
 	private final int price;
 
-	public MenuItem(String name, int price) {
+	//コンストラクタで実際にインスタンスを産む際を定義
+	public MenuItem( String name, int price){
 		this.name = name;
 		this.price = price;
 	}
 
-	public String getName() {
+	//ゲッターでしかとれないように設定
+	public String getName(){
 		return name;
 	}
 
-	public int getPrice() {
+	public int getPrice(){
 		return price;
 	}
 }
