51fa0b08e06a1b5ecb8531a41399a77b20a9fa4a
akihide-hatano <a.h.19841202@gmail.com>
2026-04-29 08:33:19 +0900

    import分1行削除

--- src/test/java/service/LocalMenuServiceTest.java
+++ src/test/java/service/LocalMenuServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import model.YakinikuyaGroup;
 import model.Restaurant;
 
 public class LocalMenuServiceTest {
